737461b6168333ad67e8e9735af71edac19ea1c6
Aurélio L. Andrade <aurelio@sistemico.com.br>
2020-08-18 15:16:19 -0300

    Utilizacao apenas do JsonIgnore ao invés JasonManagedReference e BackReference

--- src/main/java/com/mestresistemico/cursomc/domain/Endereco.java
+++ src/main/java/com/mestresistemico/cursomc/domain/Endereco.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Endereco {
@@ -24,7 +24,7 @@ public class Endereco {
 	@JoinColumn(name="cidade_id")
 	private Cidade cidade;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
--- src/main/java/com/mestresistemico/cursomc/domain/Pagamento.java
+++ src/main/java/com/mestresistemico/cursomc/domain/Pagamento.java
@@ -8,7 +8,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mestresistemico.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
@@ -20,7 +20,7 @@ public abstract class Pagamento {
 	private Integer id;
 	private Integer estado;
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId
